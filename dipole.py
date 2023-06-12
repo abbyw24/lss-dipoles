12e773b7372829f22a8e85f02fda85a233c2e81a
Abby Williams <aew492@log-3.nyu.cluster>
2023-06-12 16:42:43 -0400

    function for expected dipole map

--- dipole.py
+++ dipole.py
@@ -3,6 +3,7 @@ Get the expected number-count dipole in a healpy map.
 """
 
 import numpy as np
+import healpy as hp
 
 def radec_to_thetaphi(ra, dec):
     theta = ra * np.pi/180
@@ -19,3 +20,12 @@ def dipole(ra, dec, dipamps):
     dx, dy, dz = dipamps
     theta, phi = radec_to_thetaphi(ra, dec)
     return dx*np.sin(theta)*np.cos(phi) + dy*np.sin(theta)*np.sin(phi) + dz*np.cos(theta)
+
+def dipole_map(amps, NSIDE=64):
+    """Generate a healpy dipole map (equatorial coordinates) given four parameters:
+       the monopole plus three dipole amplitudes."""
+    NPIX = hp.nside2npix(NSIDE)  # number of pixels
+    theta, phi = hp.pix2ang(NSIDE, ipix=np.arange(NPIX))  # get (theta,phi) coords of each pixel
+    ra, dec = thetaphi_to_radec(theta, phi)  # convert (theta,phi)->(RA,Dec)
+    dip = dipole(ra, dec, amps[1:])  # expected dipole: shape==(NPIX,)
+    return amps[0] + dip
