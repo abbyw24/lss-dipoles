21fe6c88d727364b017bfffc691cef8641f8ffe8
Abby Williams <abby.e.williams@nyu.edu>
2023-09-01 17:47:27 -0400

    add dipole functions

--- dipole.py
+++ dipole.py
@@ -1,15 +1,17 @@
-"""
-Get the expected number-count dipole in a healpy map.
-"""
-
 import numpy as np
 import healpy as hp
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 
-## COORDINATE TRANSFORMATIONS ##
+import tools
+
+
+### COORDINATE TRANSFORMATIONS
+
 def xyz_to_thetaphi(xyz):
-    """Given (x,y,z), return (theta,phi) coordinates on the sphere, where phi=LON and theta=LAT."""
+    """
+    Given (x,y,z), return (theta,phi) coordinates on the sphere, where phi=LON and theta=LAT.
+    """
     x, y, z = xyz
     r = np.linalg.norm(xyz)
     theta = np.arccos(z/r)
@@ -23,30 +25,154 @@ def spherical_to_cartesian(r, theta, phi):
     return np.array([x, y, z])
 
 
-## DIPOLE CONTRIBUTIONS ##
+
+### ELLIS & BALDWIN APPROXIMATION
+
+def EllisBaldwin(x, alpha, v=369.825*u.km/u.s):
+    """
+    Return the expected dipole amplitude from Ellis & Baldwin (1984), given
+        x = number-count slope at flux density limit
+        alpha = source spectral index assuming power law spectra
+    """
+    return v / const.c.to(u.km/u.s) * (2 + x * (1+alpha))
+
+
+
+### DIPOLE CONTRIBUTIONS
+
 def dipole(theta, phi, dipole_x, dipole_y, dipole_z):
-    """Return the signal contribution from the dipole at a certain sky location (theta,phi)."""
+    """
+    Return the signal contribution from the dipole at a certain sky location (theta,phi).
+    """
     return dipole_x*np.sin(theta)*np.cos(phi) + dipole_y*np.sin(theta)*np.sin(phi) + dipole_z*np.cos(theta)
 
 def dipole_map(amps, NSIDE=64):
-    """Generate a healpy dipole map (equatorial coordinates) given four parameters:
-       the monopole plus three dipole amplitudes."""
+    """
+    Generate a healpix dipole map (equatorial coordinates) given four parameters:
+       the monopole plus three dipole amplitudes.
+    """
     NPIX = hp.nside2npix(NSIDE)  # number of pixels
     theta, phi = hp.pix2ang(NSIDE, ipix=np.arange(NPIX))  # get (theta,phi) coords of each pixel
     dip = dipole(theta, phi, *amps[1:])  # expected dipole: shape==(NPIX,)
     return amps[0] + dip
 
+def fit_dipole(map_to_fit, Cinv=None, fit_zeros=False, idx=None):
+    """
+    Perform a least-squares fit to the dipole in a healpix map.
+
+    Parameters
+    ----------
+    map_to_fit : healpix map
+        Map of pixel values to fit.
+    cov : array-like, optional
+        Covariance matrix to use in the fit. If None, identity is used.
+    fit_zeros : bool
+        Whether to fit pixels in the map with value zero.
+    idx : array-like, optional
+
+
+    Returns
+    -------
+    bestfit_pars : array-like
+        Monopole + three dipole components.
+    bestfit_stderr : float
+        Uncertainties on the best-fit amplitudes.
+    """
+
+    assert map_to_fit.ndim == 1, "input map must be 1-dimensional"
+
+    NPIX = len(map_to_fit)
+
+    # 3 orthogonal dipole template maps
+    template_amps = np.column_stack((np.zeros(3), np.diag(np.ones(3))))  # first column for the monopole
+    template_maps = np.array([dipole_map(amps, NSIDE=hp.npix2nside(NPIX)) for amps in template_amps])
+
+    # design matrix
+    A = np.column_stack((np.ones(NPIX), template_maps.T))
+    # covariances: identity for now
+    if Cinv is None:
+        Cinv = np.ones(NPIX)
+    else:
+        assert len(Cinv) == NPIX, "input Cinv and input map must have the same length"
+
+    # indices to fit
+    idx_to_fit = np.full(NPIX, True)
+    if fit_zeros is False:
+        idx_to_fit = idx_to_fit & (map_to_fit!=0.)
+    if idx is not None:
+        assert len(idx) == NPIX, "input idx and input map must have the same length"
+        idx_to_fit = idx_to_fit & idx
+    map_to_fit, A, Cinv = map_to_fit[idx_to_fit], A[idx_to_fit], Cinv[idx_to_fit]
+
+    # perform the regression
+    bestfit_pars, bestfit_Cinv = tools.lstsq(map_to_fit, A, Cinv)
+
+    # uncertainties on the best-fit pars
+    bestfit_stderr = np.sqrt(np.diag(np.linalg.inv(bestfit_Cinv)))
+
+    return bestfit_pars, bestfit_stderr
+
+
+def getDipoleVectors_healpy(densitymap, mask=[None], galcut=0, verbose=False) :
+	"""
+    ! COPIED FROM SECREST !
+	Computes the preferred direction and the estimated dipole amplitude from a density map
+	
+	This is a wrapper for the healpy routine fit_dipole
+	"""
+	
+	if mask[0] != None :
+		densitymap[(mask == 0)] = np.nan
+	
+	residual,monopole,dipole = hp.remove_dipole(densitymap,bad=np.nan,fitval=True,gal_cut=galcut,verbose=verbose)
+	norm = np.sqrt(np.dot(dipole,dipole))
+	dipole_norm = dipole/norm
+	
+	d = norm/monopole
+	
+	return dipole_norm,d
+
+
 def cmb_dipole(frame='icrs', amplitude=0.007):
-    """Return the orthogonal (x,y,z) CMB dipole components."""
+    """
+    Return the orthogonal (x,y,z) CMB dipole components.
+    """
     cmb_dipdir = SkyCoord(264, 48, unit=u.deg, frame='galactic')
     if frame=='icrs':
-        amps = np.array([0., *spherical_to_cartesian(r=amplitude,
+        amps = np.array([1., *spherical_to_cartesian(r=amplitude,
                                              theta=np.pi/2-cmb_dipdir.icrs.dec.rad,
                                              phi=cmb_dipdir.icrs.ra.rad)])
     elif frame=='galactic':
-        amps = np.array([0., *spherical_to_cartesian(r=amplitude,
+        amps = np.array([1., *spherical_to_cartesian(r=amplitude,
                                              theta=np.pi/2-cmb_dipdir.b.rad,
                                              phi=cmb_dipdir.l.rad)])
     else:
         assert False, "unknown frame"
     return amps
+
+def get_dipole(amps, frame='icrs', verbose=False):
+    """
+    Return the amplitude and direction of a dipole given its four amplitudes.
+
+    Parameters
+    ----------
+    amps : array-like
+        monopole + 3 orthogonal dipole amplitudes
+    frame : SkyCoord-compatible coordinate frame
+        frame of the input amplitudes
+    
+    Returns
+    -------
+    amp : float
+        amplitude (norm) of the dipole components divided by the monopole
+    direction : SkyCoord
+        direction of the dipole
+    """
+    
+    amp = np.linalg.norm(amps[1:]/amps[0])
+    direction = hp.vec2dir(amps[1:])
+    direction = SkyCoord(direction[1], np.pi/2 - direction[0], frame=frame, unit='rad')
+    if verbose:
+        print(f"amp = {amp:.6f}")
+        print("direction: ", direction.galactic)
+    return amp, direction
