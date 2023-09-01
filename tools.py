405aef03236dbee0f85b48e7f5396166f130deaa
Abby Williams <abby.e.williams@nyu.edu>
2023-09-01 17:48:04 -0400

    fix mollview function

--- tools.py
+++ tools.py
@@ -3,6 +3,7 @@ Helper functions.
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 import astropy
 from astropy.table import Table
 from astropy.coordinates import SkyCoord
@@ -10,13 +11,14 @@ import os
 import healpy as hp
 from healpy.newvisufunc import projview
 
+
 def load_catalog_as_map(catalog, frame='icrs', NSIDE=64, dtype=float):
     if type(catalog)==str:
         tab = Table.read(catalog, format='fits')
     elif type(catalog)==astropy.table.table.Table:
         tab = catalog
     else:
-        assert TypeError, "invalid input catalog type (filename or astropy table)"
+        raise TypeError("invalid input catalog type (filename or astropy table)")
     if frame=='galactic':
         try:
             lon, lat = tab['l'], tab['b']
@@ -31,6 +33,7 @@ def load_catalog_as_map(catalog, frame='icrs', NSIDE=64, dtype=float):
     hpmap = np.bincount(pix_idx, minlength=hp.nside2npix(NSIDE))
     return hpmap.astype(dtype)
 
+
 def get_galactic_mask(blim, NSIDE=64, frame='icrs'):
     """Returns a HEALPix mask around the galactic plane given an absolute b (latitude) limit."""
     NPIX = hp.nside2npix(NSIDE)
@@ -40,6 +43,7 @@ def get_galactic_mask(blim, NSIDE=64, frame='icrs'):
     galactic_mask = np.full(NPIX, True)
     galactic_mask[idx_to_cut] = False
     return galactic_mask
+
 
 def flatten_map(sf_map):
     newarr = np.array([row[0] for row in sf_map])
@@ -52,10 +56,12 @@ def plot_map(map, projection_type='mollweide', coord=['C'],
              graticule=graticule, graticule_labels=graticule_labels, **kwargs)
 
 
-def mollview(map, coord=['C'], graticule=True, graticule_labels=True, title=None, **kwargs):
-    hp.mollview(map, coord=coord, title=title, **kwargs)
+def mollview(map, coord=['C'], graticule=True, graticule_coord=None, graticule_labels=True,
+            title=None, unit='number density per healpixel', figsize=None, **kwargs):
+    hp.mollview(map, coord=coord, title=title, unit=unit, **kwargs)
     if graticule:
-        hp.graticule(coord=coord)
+        gratcoord = graticule_coord if graticule_coord else None
+        hp.graticule(coord=gratcoord)
 
 
 def lstsq(Y, A, Cinv):
