ed9ea0dfba7b61a30439c402087976a1eb696a6f
Abby Williams <abby.e.williams@nyu.edu>
2024-05-21 04:49:10 -0400

    update estimate_shot_noise.py with new QSOSample() class

--- estimate_shot_noise.py
+++ estimate_shot_noise.py
@@ -2,8 +2,6 @@ import numpy as np
 import healpy as hp
 # from mpi4py import MPI
 import multiprocessing as mp
-import fitsio
-from astropy.coordinates import SkyCoord
 import astropy.units as u
 import random
 import time
@@ -12,94 +10,104 @@ import os
 import sys
 
 import tools
-from Secrest_dipole import SecrestDipole
-from multipoles import compute_Cells_from_alms_fit
-import dipole
-from jackknife import get_longitude_subsamples, compute_jackknife_uncertainty
+from qso_sample import QSOSample
+from multipoles import compute_Cells_in_overdensity_map
 
 def main():
 
     s = time.time()
 
-    print(mp.cpu_count(), flush=True)
-
     """INPUT PARAMETERS"""
     nside = 64
-    max_ell = 10
-    ntrials = 100
+    max_ell = 5
+    ntrials = 20
     Wmasks = np.logspace(-2,0,10)
-    # catalog_dict = dict(initial_catfn='catwise_agns_master.fits',
-    #                     catname='catwise_agns',
-    #                     mag='w1',
-    #                     blim=30,
-    #                     maglim=16.4,
-    #                     save_tag='_r1.0',
-    #                     load_init=True,
-    #                     NSIDE=nside)
-    catalog_dict = dict(initial_catfn='quaia_G20.0.fits',
-                        catname='quaia',
-                        mag='G',
-                        blim=30,
-                        maglim=20.,
-                        save_tag='_r1.0',
-                        load_init=True,
-                        compcorrect=False,
-                        NSIDE=nside)
 
-    # where to save
-    save_dir = os.path.join(f'/scratch/aew492/quasars/noise_Cells', catalog_dict['catname'])
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    save_fn = os.path.join(save_dir, f'noise_Cells_ellmax{int(max_ell)}_{ntrials}trials.npy')
-
-    """DATA"""
     # instantiate a dipole object
     print(f"creating dipole object")
-    d = SecrestDipole(**catalog_dict)
+    catdir = '/scratch/aew492/quasars/catalogs'
+    # d = QSOSample(initial_catfn=os.path.join(catdir, 'catwise_agns/catwise_agns_master.fits'),
+    #                     mask_fn=os.path.join(catdir, 'masks/mask_master_hpx_r1.0.fits'),
+    #                     mag='W1', maglim=16.4,
+    #                     blim=30,
+    #                     NSIDE=nside)
+    d = QSOSample(initial_catfn=os.path.join(catdir, 'quaia/quaia_G20.5.fits'),
+                    mask_fn=os.path.join(catdir, 'masks/mask_master_hpx_r1.0.fits'),
+                    mag='G', maglim=20.,
+                    blim=30,
+                    NSIDE=nside)
+    d.cut_galactic_plane()
     d.cut_mag()
-    # mean density
-    mask, mu = get_mask_and_mean_density(d)
+
+    # selection function
+    selfunc_fn = os.path.join(catdir, f'quaia/selfuncs/selection_function_NSIDE{nside}_G20.0_blim15.fits')
+    # selfunc_fn = os.path.join(catdir, f'catwise_agns/selfuncs/selection_function_NSIDE{nside}_catwise_pluszodis.fits')
+    selfunc = d.get_selfunc(selfunc=selfunc_fn)
+
+    # where to save
+    save_dir = os.path.join(f'/scratch/aew492/quasars/noise_Cells/quaia')
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    def save_fn(tag):  # tag to specify either full or cut sky
+        return os.path.join(save_dir, f'noise_Cells_{tag}_ellmax{int(max_ell)}_{ntrials}trials.npy')
+
+    # MULTIPROCESSING
+    nprocesses = 10 # os.cpu_count()
+    print(f"using {nprocesses} processes", flush=True)
+
+    """GET MASK AND MEAN SOURCE DENSITY"""
+    # get masked datamap
+    masked_datamap = d.construct_masked_datamap(selfunc=selfunc, return_map=True)
+    # mask and mean density
+    mask = d.mask
+    mu = np.nanmean(masked_datamap)
     print(f"mean density in the masked map = {mu:.2f} quasars per healpixel", flush=True)
 
+    # comment to save along with results
+    comment = 'Quaia G<20.0, masking completeness < 0.5; ' + \
+                'correcting by completeness to estimate mean source density mu, but NOT weighting by completeness in lstsq'
 
-    """COMPUTE ANGULAR POWER SPECTRA"""
-    # measure Cells on the full sky
-    print(f"computing Cells on the full sky", flush=True)
-    Cells_fullsky_trials = np.empty((ntrials, max_ell))
-    for i in range(ntrials):
-        print(f"trial {i+1} of {ntrials}\t") #, end='\r')
-        # noise realization for this trial, with the mean density, on the full sky
-        noise = np.random.poisson(mu, hp.nside2npix(nside)).astype(float)
-        # map to fit is the overdensities
-        map_to_fit = noise / np.nanmean(noise) - 1
-        # fit to spherical harmonics templates
-        ells, Cells_fullsky = compute_Cells_from_alms_fit(map_to_fit, np.ones_like(map_to_fit), max_ell)
-        Cells_fullsky_trials[i] = Cells_fullsky[1:]  # cut out monopole since we're fitting overdensities
-    Cells_fullsky = np.mean(np.array(Cells_fullsky_trials), axis=0)
+    """
+    COMPUTE ANGULAR POWER SPECTRA
+    """
+    # """FULL SKY"""
+    # print(f"computing Cells on the full sky", flush=True)
+    # pool = mp.Pool(processes=nprocesses)
+    # Cells_fullsky_trials = np.array([
+    #     pool.apply(compute_Cells_fullsky, args=(i, mu, nside, max_ell,)) for i in range(ntrials)
+    # ])
+    # Cells_fullsky = np.nanmean(Cells_fullsky_trials, axis=0)
+    # print(Cells_fullsky.shape, Cells_fullsky)
 
-    # measure Cells on the cut sky
+    # results_dict = dict(ells=np.arange(1, max_ell+1),
+    #                     Cells_fullsky=Cells_fullsky,
+    #                     ntrials=ntrials,
+    #                     selfunc_fn=selfunc_fn,
+    #                     mu=mu,
+    #                     comment=comment)
+    # np.save(save_fn('fullsky'), results_dict)
+    # print(f"saved to {save_fn('fullsky')}", flush=True)
 
-    # define function to compute Cells on a cut sky as a function of Wmask
+
+    """CUT SKY"""
+    # define function (for multiprocessing) to compute Cells on a cut sky as a function of Wmask
     def compute_Cells_cutsky(index, Wmask, results_dict):
         Cells_this_Wmask_trials = np.empty((ntrials, max_ell))
         for i in range(ntrials):
-            print(f"Wmask = {Wmask:.2e}: trial {i+1} of {ntrials}\t") #, end='\r')
-            # noise realization for this trial, with the mean density, on the full sky
+            print(f"Wmask = {Wmask:.2e}: trial {i+1} of {ntrials}")
+            # noise realization for this trial, MASKED, with the mean density, on the full sky
             noise = np.random.poisson(mu, hp.nside2npix(nside)).astype(float)
-            # MASK the same pixels as in the data
             noise[~mask] = np.nan
-            # map to fit is teh overdensities
+            # map to fit is the overdensities
             map_to_fit = noise / np.nanmean(noise) - 1
-            map_to_fit[~mask] = 0.
-            # Cinv: completeness (assumed perfect) in unmasked pixels, this Wmask in the masked pixels
-            Cinv_ = np.ones_like(map_to_fit)
-            Cinv_[~mask] = Wmask
             # fit to spherical harmonics templates
-            ells, Cells_ = compute_Cells_from_alms_fit(map_to_fit, Cinv_, max_ell)
+            ells, Cells_ = compute_Cells_in_overdensity_map(map_to_fit, Wmask=Wmask, max_ell=int(max_ell))
             Cells_this_Wmask_trials[i] = Cells_[1:]  # cut out monopole since we're fitting overdensities
         results_dict[index] = (Wmask, np.nanmean(Cells_this_Wmask_trials, axis=0))
 
-    # multiprocessing pool
+    # multiprocessing Process
+    print(f"computing Cells on the cut sky...", flush=True)
+    s = time.time()
     manager = mp.Manager()
     procs = []
     cutsky_results = manager.dict()
@@ -111,60 +119,34 @@ def main():
     for proc in procs:
         proc.join()
 
-    # !! hacky comments
-    quaia_comment = 'Quaia G<20.0, using only selfunc > 0.5'
-    catwise_comment = 'CatWISE'
-    info = quaia_comment if catalog_dict['catname'] == 'quaia' else catwise_comment
-
     results_dict = dict(ells=np.arange(1, max_ell+1),
-                        Cells_fullsky=Cells_fullsky,
                         Cells_cutsky=cutsky_results.values(),
-                        info=info)
-    np.save(save_fn, results_dict)
+                        selfunc_fn=selfunc_fn,
+                        mu=mu,
+                        comment=comment)
+    np.save(save_fn('cutsky'), results_dict)
+    print(f"saved to {save_fn('cutsky')}", flush=True)
 
     total_time = time.time()-s 
     print(f"total time = {datetime.timedelta(seconds=total_time)}")
 
 
-def get_mask_and_mean_density(d, nside=64, min_completeness=0.5):
+def compute_Cells_fullsky(itrial, mu, nside, max_ell, verbose=True):
+    if verbose:
+        print(f"starting trial {int(itrial+1)}", flush=True)
+    # noise realization for this trial, with the mean density, on the full sky
+    noise = np.random.poisson(mu, hp.nside2npix(int(nside))).astype(float)
+    # map to fit is the overdensities
+    map_to_fit = noise / np.mean(noise) - 1
+    assert np.sum(np.isnan(map_to_fit)) == 0.  # in this case Wmask is not used !
+    # fit to spherical harmonics templates
+    ells, Cells_fullsky = compute_Cells_in_overdensity_map(map_to_fit, Wmask=0., max_ell=int(max_ell))
+    return Cells_fullsky[1:]  # cut out monopole since we're fitting overdensities
 
-    # construct map from source density table
-    datamap = tools.load_catalog_as_map(d.table, NSIDE=nside)
 
-    # delete the tables to clear up memory !
-    del d.initial_cattab
-    del d.table
+def compute_Cells_fullsky_wrapper(result_list, itrial, mu, nside, max_ell, verbose=True):
 
-    # mask the galactic plane (mask pixels based on their central sky coordinate)
-    lon, lat = hp.pix2ang(nside, np.arange(hp.nside2npix(nside)), lonlat=True)
-    b = SkyCoord(lon * u.deg, lat * u.deg, frame='icrs').galactic.b
-    gal_plane_mask = np.zeros_like(datamap)
-    gal_plane_mask[np.abs(b.deg) >= d.blim] = 1
-
-    # load smaller masks (used in S21): at the pixel level
-    small_masks = fitsio.read(d.mask_fn)
-    assert len(small_masks) == len(gal_plane_mask) == len(datamap)
-
-    # combine these two into a single mask
-    mask = gal_plane_mask.astype(bool) & small_masks.astype(bool)
-
-    # completeness in each pixel
-    if d.catname == 'quaia':
-        # and load selection function
-        completeness = d.load_selfunc()
-    else:
-        assert d.catname == 'catwise_agns', "input `catname` must be 'quaia' or 'catwise_agns'"
-        # if selfunc is not provided, assume perfect completeness
-        completeness = np.ones_like(datamap)
-
-    # mask the data: populate a nan map with the data values in unmasked pixels
-    masked_datamap = np.multiply(datamap, mask, where=(mask!=0), out=np.full_like(datamap, np.nan))
-
-    # mean density of the map
-    final_mask = (completeness > min_completeness) & (mask != 0.)
-    mu = np.nansum((masked_datamap * completeness)[final_mask]) / np.nansum((completeness * completeness)[final_mask])
-
-    return final_mask, mu
+    result_list.append(compute_Cells_fullsky(itrial, mu, nside, max_ell, verbose))
 
 
 if __name__ == '__main__':
