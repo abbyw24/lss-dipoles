333f3aee835bb03a7d4a402e7755ef0847929e49
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2024-08-14 16:36:35 +0200

    initial plotting script for dipole amps vs lambda

---
+++ code/plotter.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from pathlib import Path
+
+import generate_mocks as gm
+
+
+### MATPLOTLIB SETTINGS
+import matplotlib
+matplotlib.rcParams['xtick.labelsize'] = 16 
+matplotlib.rcParams['ytick.labelsize'] = 16
+matplotlib.rcParams['axes.labelsize'] = 18
+matplotlib.rcParams['legend.fontsize'] = 14
+matplotlib.rcParams['axes.titlesize'] = 20
+matplotlib.rcParams['lines.linewidth'] = 2
+
+
+def main():
+
+    dir_figs = '../figures/figures_2024-08-14'
+    Path.mkdir(Path(dir_figs), exist_ok=True, parents=True)
+
+    #set_name = 'full'
+    set_name = 'flat_quaia'
+    tag_fig = f'_{set_name}'
+    catalog_name = 'quaia_G20.0'
+    selfunc_mode = 'quaia_G20.0_orig'
+    case_dict_data = {
+        "catalog_name": catalog_name,
+        "selfunc_mode": selfunc_mode, #this also multiplies in the mask
+        "tag": f"_case-{selfunc_mode}"
+    }
+    case_dicts_mock = gm.case_set(set_name=set_name)
+    for case_dict_mock in case_dicts_mock:
+        tag_fig = f"_{catalog_name}{case_dict_mock['tag']}"
+        fn_fig = f'{dir_figs}/dipole_comps_vs_lambdas{tag_fig}.png'
+        plot_dipole_comps_vs_lambdas(case_dict_data, case_dict_mock, 
+                                    title=tag_fig[1:], fn_fig=fn_fig)
+        break
+
+
+def plot_dipole_comps_vs_lambdas(#fn_comps_data, fns_comps_mocks, 
+                                 case_dict_data, case_dict_mock,
+                                 label_data='Data', label_mock='Mock',
+                                 title='', fn_fig=None):
+
+    dir_results_data = '../results/results_data'
+    dir_results_mocks = '../results/results_mocks'
+
+    # Load data
+    fn_comps_data = os.path.join(dir_results_data, f"lambda_comps_{case_dict_data['catalog_name']}{case_dict_data['tag']}.npy")
+    result_dict = np.load(fn_comps_data, allow_pickle=True).item()
+    dipole_comps_data = result_dict['dipole_comps']
+    lambdas_data = result_dict['Lambdas']
+
+    # Load mock
+    lambdas_mocks = []
+    dipole_amps_mocks = []
+    n_trials = 12
+    for i in range(n_trials):
+        fn_comps_mock = os.path.join(dir_results_mocks, f"lambda_comps_mock{case_dict_mock['tag']}_trial{i}.npy")
+        result_dict = np.load(fn_comps_mock,  allow_pickle=True).item()
+        dipole_amps_mock = np.linalg.norm(result_dict['dipole_comps'], axis=-1)
+        dipole_amps_mocks.append(dipole_amps_mock)
+        lambdas_mocks.append(result_dict['Lambdas'])
+    dipole_amps_mocks = np.array(dipole_amps_mocks)
+
+    # Compute the norm of the dipole components for the actual data
+    dipole_amps_data = np.linalg.norm(dipole_comps_data, axis=1)
+
+    # Plot data
+    plt.figure(figsize=(10, 6))
+    plt.plot(lambdas_data, dipole_amps_data, lw=3, color='k', label=label_data)
+
+    # Plot each mock trial with light red lines
+    for i in range(n_trials):        
+        plt.plot(lambdas_mocks[i], dipole_amps_mocks[i], color='lightcoral', linewidth=1)
+
+    # Plot the mean of the mock data with a dark red line
+    dipole_amps_mock_mean = np.mean(dipole_amps_mocks, axis=0)
+    plt.plot(lambdas_mocks[0], dipole_amps_mock_mean, color='darkred', linewidth=3, label=label_mock)
+
+    # Adding labels and legend
+    plt.xscale('log')
+
+    plt.xlabel(r'$\Lambda$')
+    plt.ylabel(r'$\mathcal{D}$, dipole amplitude')
+    plt.title(title)
+    plt.legend()
+
+    if fn_fig is not None:
+        plt.savefig(fn_fig, bbox_inches='tight')
+        print(f"Saved figure to {fn_fig}")
+
+
+if __name__ == '__main__':
+    main()
