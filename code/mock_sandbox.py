e0c627be5e7c2f209fa33b9575984fb8f4f3188f
Abby Williams <abby.e.williams@nyu.edu>
2025-02-02 03:39:10 -0500

    add 'sandbox' to loop through accepted ABC samples

---
+++ code/mock_sandbox.py
@@ -0,0 +1,92 @@
+import numpy as np
+import os
+import sys
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+import healpy as hp
+
+import tools
+from abc_with_fake_data import model
+import multipoles
+import dipole
+
+def main():
+
+    """
+    Run some function on a list of mocks.
+    """
+
+    # inputs
+    # dipamp = 0.0052
+    # excess = '1e-4'
+
+    base_rate = 33.633 # for Quaia , 77.4495 # for Catwise
+    nmocks = 500
+    ngens = 20
+    ell_max = 8
+
+    # regularization for the Cells
+    Lambda = 0.
+
+    n = 500  # number to actually compute (will take the first n)
+    overwrite = False
+
+    # info to load
+    result_dir = os.path.join('/scratch/aew492/lss-dipoles_results/results/ABC/',
+                                f'quaia_dipole_excess_nside1_{nmocks}mocks_{ngens}iters_base-rate-{base_rate:.4f}')
+                                    # f'fake_data/dipole-{dipamp:.4f}_excess-{excess}_base-rate-{base_rate:.4f}',
+                                    # f'{nmocks}mocks_{ngens}gens')
+    posterior_dir = os.path.join(result_dir, 'accepted_samples')
+    fn_list = [
+        os.path.join(posterior_dir, f'mock{i}.npy') for i in range(n)
+    ]
+
+    # where to save
+    save_dir = os.path.join(result_dir, 'accepted_samples_Cells')
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    save_fn_list = [
+        os.path.join(save_dir, f'Cells_mock{i}.npy') for i in range(n)
+    ]
+
+    # load the ABC results
+    resdict = np.load(os.path.join(result_dir, f'results.npy'), allow_pickle=True).item()
+    try:
+        selfunc = resdict['selfunc']
+    except KeyError:
+        print(f"'selfunc' key not found; assuming perfect completeness")
+        selfunc = np.ones_like(np.load(fn_list[i])).astype(float)
+
+    """
+    loop through the mocks
+    """
+
+    for i, fn in enumerate(fn_list):
+        print(f"mock {i+1:01d} of {len(fn_list)}, {(i+1)/len(fn_list) * 100:.0f}%", end='\r', flush=True)
+        # does this save file already exist?
+        if os.path.exists(save_fn_list[i]) and overwrite == False:
+            continue
+
+        # load the mock
+        mock = np.load(fn).astype(float)
+
+        # compute the Cells on the mock:
+        # convert mock to overdensities
+        odmap = dipole.overdensity_map(mock, selfunc)
+        ells, Cells, alms = multipoles.compute_Cells_in_overdensity_map_Lambda(odmap,
+                                                                Lambda=Lambda,
+                                                                max_ell=ell_max,
+                                                                selfunc=selfunc,
+                                                                return_alms=True)
+
+        # package results
+        res = dict(ells=ells, Cells=Cells, alms=alms, Lambda=Lambda)
+
+        # save
+        np.save(save_fn_list[i], res)
+
+    print("done!")
+
+
+if __name__ == '__main__':
+    main()
--- code/sandbox.py
+++ code/sandbox.py
@@ -1,26 +1,91 @@
 import numpy as np
 import os
 import sys
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+import healpy as hp
 
-import dipole
-from qso_sample import QSOSample
 import tools
-
+from abc_with_fake_data import model
 
 def main():
 
-    # instantiate QSO sample
-    catdir = '/scratch/aew492/quasars/catalogs'
-    nside = 64
-    d = QSOSample(initial_catfn=os.path.join(catdir, 'quaia/quaia_G20.5.fits'),
-                    mask_fn=os.path.join(catdir, 'masks/mask_master_hpx_r1.0.fits'),
-                    mag='G', maglim=20.,
-                    blim=30,
-                    NSIDE=nside)
-    selfunc_fn = os.path.join(catdir, f'quaia/selfuncs/selection_function_NSIDE{nside}_G20.0_blim15.fits')
+    """
+    Smooth a set of posterior and prior mock quasar maps from an ABC result.
+    """
 
-    # test new galactic plane cut
-    d.cut_galactic_plane_hpx()
+    # inputs
+    dipamp = 0.0052
+    excess = '1e-4'
+    base_rate = 33.633
+    nmocks = 500
+    ngens = 15
+    nside = 64
+    sr = 1
+
+    n_to_smooth = 500  # will take the first n mocks from the accepted samples, and generate(&smooth) n mocks from the prior
+    overwrite = True
+
+    # directory things
+    result_dir = os.path.join('/scratch/aew492/lss-dipoles_results/results/ABC/fake_data',
+                                    f'dipole-{dipamp:.4f}_excess-{excess}_base-rate-{base_rate:.4f}',
+                                    f'{nmocks}mocks_{ngens}gens')
+    posterior_dir = os.path.join(result_dir, 'accepted_samples')
+    prior_dir = os.path.join(result_dir, 'prior_samples')
+    if not os.path.exists(prior_dir):
+        os.makedirs(prior_dir)
+    post_mocks_fn_list = [
+        os.path.join(posterior_dir, f'mock{i}') for i in range(n_to_smooth)
+    ]
+    prior_mocks_fn_list = [
+        os.path.join(prior_dir, f'mock{i}') for i in range(n_to_smooth)
+    ]
+
+    post_mocks_to_generate = post_mocks_fn_list.copy()
+    prior_mocks_to_generate = prior_mocks_fn_list.copy()
+
+    # do the smoothed mocks exist?
+    if overwrite == False:
+        for i in range(n_to_smooth):
+            if os.path.exists(post_mocks_fn_list[i]+'_smoothed.npy'):
+                post_mocks_to_generate.remove(post_mocks_fn_list[i])
+            if os.path.exists(prior_mocks_fn_list[i]+'_smoothed.npy'):
+                prior_mocks_to_generate.remove(prior_mocks_fn_list[i])
+
+    # load the ABC results
+    resdict = np.load(os.path.join(result_dir, f'results.npy'), allow_pickle=True).item()
+
+    """
+    get mocks from the prior and posterior
+    """
+
+    # ## POSTERIOR ##
+    # print("smoothing the posterior mocks")
+    # for i, fn in enumerate(post_mocks_to_generate):
+    #     print(f"{i+1} of {len(post_mocks_to_generate)}")
+    #     mock = np.load(fn+'.npy')
+    #     smoothed_mock = tools.smooth_map(mock, sr=sr)
+    #     np.save(fn+'_smoothed.npy', smoothed_mock)
+
+
+    ## PRIOR ##
+
+    # required inputs for the sky model
+    selfunc = np.ones(hp.nside2npix(nside))
+    dipdir = SkyCoord(264, 48, unit=u.deg, frame='galactic')
+    theta, phi = hp.pix2ang(nside, ipix=np.arange(hp.nside2npix(nside)))
+    model_args = dict(base_rate=base_rate, selfunc=selfunc, dipdir=dipdir, theta=theta, phi=phi, ell_max=resdict['ell_max'])
+
+    prior_mocks = tools.generate_mocks_from_prior(resdict['prior'], model, len(prior_mocks_to_generate), nside, **model_args)
+    for i, fn in enumerate(prior_mocks_to_generate):
+        np.save(fn, prior_mocks[i])
+    # for i, fn in enumerate(prior_mocks_to_generate):
+    #     print(f"{i+1} of {len(prior_mocks_to_generate)}")
+    #     smoothed_mock = tools.smooth_map(prior_mocks[i], sr=sr)
+    #     np.save(fn+'_smoothed.npy', smoothed_mock)
+
+    print("done!")
+
 
 if __name__ == '__main__':
     main()
